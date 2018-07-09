2996d3a7e80bdafbbc643ef58c35d38cceed0ef1
EricSDavis <ericscottdavis@outlook.com>
2018-07-09 17:12:34 -0400

    Building App

--- hic2probes_app/server.R
+++ hic2probes_app/server.R
@@ -1,17 +1,84 @@
 library(shiny)
 
 # Define server logic
-shinyServer(function(input, output) {
+shinyServer(function(input, output, session) {
   
-  eventReactive(input$test, {
-    system("./hic2probes/shell/hicsq.sh -e 133100000 -n 10", input = "yes")
-    data <- as.data.frame(read.delim("../hic2probes/output/probes.bed"))
+  ##------------Switch to Evalutate Page-----------####
+  observeEvent(input$run_script, {
+    newtab <- switch(input$tabNav,
+                     "Define" = "Evaluate",
+                     "Evaluate" = "Define"
+    )
+    updateTabItems(session, "tabNav", newtab)
+  })
+  
+  ##------------Return to Define Page--------------####
+  observeEvent(input$return, {
+    newtab <- switch(input$tabNav,
+                     "Evaluate" = "Define",
+                     "Define" = "Evaluate"
+                     
+    )
+    updateTabItems(session, "tabNav", newtab)
+    removeUI(selector = "div.loader")
+    insertUI(selector = "div#spinner",
+             ui = conditionalPanel(
+               condition = "input.run_script",
+               HTML('
+                    <div id="spinner" class="loader"></div>
+                    ')
+               )
+             )
+  })
+  
+  ##-------------Run script-----------------------####
+  observeEvent(input$run_script, {
+    withProgress(message = "Constructing Probes ...", {
+      
+      message <- "There were no errors!"
+      
+      if(input$stop < input$start){
+        message <- "Invalid option: Start must be less than stop"
+      }
+      
+      ## Handle NA values for input$max probes ####
+      if (is.na(input$max_probes)){
+        max_probes <- ""
+      } else {
+        max_probes <- paste0(" -n ", input$max_probes)
+      }
+      
+      ## Stitch command ####
+      command <- paste0("./../hic2probes/shell/hicsq.sh",
+                        " -c ", input$chr,
+                        " -b ", input$start,
+                        " -e ", input$stop,
+                        " -r ", input$resenz,
+                        max_probes)
+      system(command, input = "yes")
+    })
+    
+    ## Test messages ####
+    output$test <- renderText({
+      message
+    })
+  })
+  
+  ## Get script results in reactive context ####
+  script_results <- eventReactive(input$run_script, {
+    data <- as.data.frame(read.delim("../hic2probes/output/probes.bed", header = F))
+    colnames(data) <- c("chr", "start", "stop", "shift", "res.fragment", "dir", "AT", "GC", "seq", "pass")
     data
   })
   
+  ##--------------Probe data table----------------####
   output$Probes <- renderDataTable({
-    stuff()
+    script_results()
   })
+
+
+
+  
   
   
   
--- hic2probes_app/ui.R
+++ hic2probes_app/ui.R
@@ -1,21 +1,151 @@
 library(shiny)
+library(shinydashboard)
 
-# Define UI
-shinyUI(fluidPage(
-  
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-      actionButton("test", "Test Script")
-    ),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-       plotOutput("distPlot"),
-       dataTableOutput("Probes")
-    )
+## Dashboard Header ####
+header <- dashboardHeader(
+  title = "LURE"
+)
+
+## Dashboard Sidebar ####
+sidebar <- dashboardSidebar(
+  collapsed = T,
+  disable = T,
+  sidebarMenu(
+    id = "tabNav",
+    menuItem("Define", tabName = "Define"),
+    menuItem("Evaluate", tabName = "Evaluate")
   )
-))
+)
+
+## Dashboard Body ####
+body <- dashboardBody(
+  
+  ## Link in stylesheets ####
+  tags$link(rel = "stylesheet", type = "text/css", href = "css/style.css"),
+  
+  ## Pages ####
+  tabItems(
+    
+    ## "Define" Page ####
+    tabItem(
+      tabName = "Define",
+
+      ## Create Sidebar Menu ####
+      fluidRow(
+        box(
+          class = "sidebar",
+          solidHeader = T,
+          status = "primary",
+          title = "DEFINE REGION",
+          width = 4,
+          
+          ## Genome Input ####
+          selectInput(
+            inputId = "genome",
+            label = "Genome:",
+            choices = list("Human" = c("hg38", "hg19", "hg18"),
+                           "Mouse" = c("GRCm38/mm10", "NCBI37/mm9"),
+                           "Rat" = c("RGSC 6.0/rn6", "RGSC 5.0/rn5"),
+                           "Fruitfly" = c(),
+                           "Worm" = c(),
+                           "Yeast" = c()
+                           ),
+            selected = "hg19",
+            multiple = F
+          ),
+          
+          ## Chromosome Input ####
+          selectInput(
+            inputId = "chr",
+            label = "Chromosome:",
+            choices = c("chr1", "chr2", "chr3", "chr4", "chr5", "chr6",
+                        "chr7", "chr8", "chr9", "chr10", "chr11", "chr12",
+                        "chr13", "chr14", "chr15", "chr16", "chr17", "chr18",
+                        "chr19", "chr20", "chr21", "chr22", "chrX", "chrY", "chrM"),
+            selected = "chr8",
+            multiple = F
+          ),
+          
+          ## Start Input ####
+          numericInput(
+            inputId = "start",
+            label = "Start:",
+            value = "133000000"
+          ),
+          
+          ## Stop Input ####
+          numericInput(
+            inputId = "stop",
+            label = "Stop:",
+            value = "133100000"
+          ),
+          
+          ## Restriction Enzyme Input ####
+          selectInput(
+            inputId = "resenz",
+            label = "Restriction Enzyme:",
+            choices = c("^GATC,MobI", "A^AGCTT,HindIII"),
+            selected = "^GATC,MobI",
+            multiple = F
+          ),
+
+          ## Number of Probes ####
+          numericInput(
+            inputId = "max_probes",
+            label = "Number of Probes:",
+            min = 0,
+            value = NA
+          ),
+          
+          ## Design Probes ####
+          div(
+            class = "rightAlign",
+            actionButton(
+              inputId = "run_script",
+              label = "Design Probes",
+              icon = icon("wrench", lib = "font-awesome")
+            )
+          ) # end of div
+          
+        ), # end of box
+        div(id="spinner"),
+        conditionalPanel(
+          condition = "input.run_script",
+          HTML('
+             <div class="loader"></div>
+               ')
+        )
+        
+      ) # end of fluidRow
+      
+    ), # end of tabItem
+    
+    
+    ## "Evaluate" Page ####
+    tabItem(
+      tabName = "Evaluate",
+      h2("This is the Evaluate Page"),
+      
+      ## Return to Start Page ####
+      actionButton(
+        inputId = "return",
+        label = "Start-Over",
+        icon = icon("arrow-left", lib = "font-awesome")
+      ),
+      
+      tags$br(),
+      tags$br(),
+      
+      ## Probe Data Table ####
+      dataTableOutput("Probes"),
+      
+      textOutput("test")
+      
+    ) # end of tabItem
+    
+  ) # end of tabItems
+  
+) # end of body
+
+## Dashboard Page ####
+dashboardPage(header, sidebar, body)
